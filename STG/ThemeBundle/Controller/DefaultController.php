3e24e0c175bd2e143bda870ab04035a6348e9508
Aguilar Lucas <laguilar@santafe.gov.ar>
2017-11-28 13:45:31 -0300

    Se agrega la carpeta STG con el código inicial del bundle

---
+++ STG/ThemeBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace STG\ThemeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('STGThemeBundle:Default:index.html.twig');
+    }
+}
