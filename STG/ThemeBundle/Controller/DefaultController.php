2c062001fc001c7a85315d27e74c8ece1a6067e0
Pradolini Fernando <fpradolini@hotmail.com>
2020-08-28 12:33:51 -0300

    Se reemplaza extend de clase Controller(deprecated) por AbstractController

--- STG/ThemeBundle/Controller/DefaultController.php
+++ STG/ThemeBundle/Controller/DefaultController.php
@@ -2,10 +2,10 @@
 
 namespace STG\ThemeBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class DefaultController extends Controller
+class DefaultController extends AbstractController
 {
     /**
      * @Route("/")
