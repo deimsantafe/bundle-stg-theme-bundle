c00dcb4b565f162eed3dac1941b87d0233ff273a
Pradolini Fernando <fpradolini@hotmail.com>
2020-09-01 09:49:07 -0300

    Ajustes para reemplazar extend de clase Controller (deprecated) por AbstractController

--- STG/ThemeBundle/Controller/DefaultController.php
+++ STG/ThemeBundle/Controller/DefaultController.php
@@ -2,27 +2,23 @@
 
 namespace STG\ThemeBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class DefaultController extends Controller
-{
+class DefaultController extends AbstractController
+{   
+
     /**
      * @Route("/")
      */
     public function indexAction()
     {
-        //dump('stg_theme.configuration.materialize: '.$this->getParameter('stg_theme.configuration.materialize'));
-
-
         if ($this->getParameter('stg_theme.configuration.materialize') == false) {
             //renderizo vista bootstrap
             return $this->render('@STGTheme/Default/base.html.twig');
         }
         else {
             //renderizo vista materializecss
-            //return $this->render('STGThemeBundle::Default:baseMaterializecss.html.twig');
-
             return $this->render('@STGTheme/Default/baseMaterializecss.html.twig');
         }
 
